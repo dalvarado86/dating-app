c70c609355923a31354d28444ea91c9d5467042e
dalvarado86 <dealvarado86@gmail.com>
2023-04-07 21:16:05 -0400

    Scaffolds angular client app

--- API/Program.cs
+++ API/Program.cs
@@ -13,9 +13,16 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString(ConnectionString));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
