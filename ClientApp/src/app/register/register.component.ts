82a7a0c2378e4fa0a5459151a5ad079802c2c79d
dalvarado86 <dealvarado86@gmail.com>
2023-07-05 22:27:15 -0400

    Adds inputs to register form

--- ClientApp/src/app/register/register.component.ts
+++ ClientApp/src/app/register/register.component.ts
@@ -24,9 +24,14 @@ export class RegisterComponent implements OnInit {
     this.initializeForm();
   }
 
-  initializeForm() {
+  initializeForm() { 
     this.registerForm = this.formBuilder.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]],
     });
